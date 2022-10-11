051dde15982870197d442f6ebbfb6fe0d1931176
Dave Fisher <dave.j.fish@gmail.com>
2022-10-11 12:48:45 -0700

    working half baked

--- functions/fetch-yelp/index.js
+++ functions/fetch-yelp/index.js
@@ -1,9 +1,36 @@
 const fetch = require('node-fetch');
 require('dotenv').config({ path: `.env.development.local` });
 
-const handler = async (event) => {
+exports.handler = async (event) => {
   // add code here to fetch data from yelp API
   // be sure to include the parameters from event.queryStringParameters
+  const zip = event.queryStringParameters.zip;
+  const term = event.queryStringParameters.term;
+
+  console.log('zip is: ', zip);
+  console.log('term is: ', term);
+
+  try {
+    const resp = await fetch(
+      `https://api.yelp.com/v3/businesses/search?categories=restaurants&location=${zip}&term=${term}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+        },
+      }
+    );
+    const data = await resp.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.businesses),
+    };
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    };
+  }
 };
 
-module.exports = { handler };
